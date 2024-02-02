5a17446ceb405266b83df6d7e538d6db22d250cf
Lukas <lukkotecki@gmail.com>
2024-02-02 08:34:04 +0100

    initial commit via CLI

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(App())
+
+
+
+
+function App (){
+    return (
+    <div className='main-container'>
+
+        <h1>This is React template</h1>
+    
+    </div>
+    )
+}
+
+
+
+
+
